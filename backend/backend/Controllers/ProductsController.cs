4ba7a49f54c91c7d3b4909798220eac472901622
jinwoo park <elvintern@gmail.com>
2023-08-28 14:38:40 +1200

    Set up the server and DB

---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using backend.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+
+        // Read
+
+        // Update
+
+        // Delete
+    }
+}
